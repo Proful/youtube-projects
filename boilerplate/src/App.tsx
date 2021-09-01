4d0efdf40bd76984ff82f30119012b2190d949e3
Proful <proful.sadangi@gmail.com>
2021-09-01 19:27:01 +1000

    Boilerplate code v1

---
+++ boilerplate/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-400">Hi from Kenny & Proful</h1>
+    </div>
+  )
+}
+
+export default App
